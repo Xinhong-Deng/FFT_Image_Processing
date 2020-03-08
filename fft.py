dd371025120c676c92cc2b34b77fa2a78deeabbf
Xinhong Deng <sandra.deng.0520@gmail.com>
2020-03-08 15:06:29 -0400

    take arguments from command line

--- fft.py
+++ fft.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 
@@ -16,3 +18,37 @@ def twodfft_inverse(signal: np.array):
         columns_inverse.append(fft_inverse(signal[:, c]))
     return 1/(row * column) * fft_inverse(columns_inverse)
 
+
+if __name__ == '__main__':
+    mode = 1
+    img = "moonlanding.png"
+    print(sys.argv)
+    index = 1
+    while index < len(sys.argv):
+        if sys.argv[index] == "-m":
+            index += 1
+            mode = sys.argv[index]
+        elif sys.argv[index] == "-i":
+            index += 1
+            img = sys.argv[index]
+        else:
+            print("error in argument")
+            exit()
+
+        index += 1
+
+    if mode == 1:
+        # call mode 1 function
+        exit()
+
+    if mode == 2:
+        # call mode 2 function
+        exit()
+
+    if mode == 3:
+        # call mode 3 function
+        exit()
+
+    if mode == 4:
+        # call mode 4 function
+        exit()
